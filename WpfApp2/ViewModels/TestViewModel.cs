a872c42a2fe02a974d42ee221f43b358389d3cdb
gclove2000 <1123448674@qq.com>
2024-03-05 09:33:59 +0800

    初始化项目

---
+++ WpfApp2/ViewModels/TestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.ViewModels
+{
+    public partial class TestViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        private string title = "默认title";
+        public TestViewModel() { }
+    }
+}
